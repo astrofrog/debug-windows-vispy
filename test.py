53bc3fc31fa9065fb02c13702d83b3e7779e3648
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-08-16 14:11:55 +0200

    Down to one file

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-import numpy as np
-from vispy_widget import QtVispyWidget
+from __future__ import absolute_import, division, print_function
+
 from PyQt4 import QtGui
+from vispy import scene
+
+import numpy as np
+
+class QtVispyWidget(QtGui.QWidget):
+
+    def __init__(self, parent=None, data=None):
+
+        super(QtVispyWidget, self).__init__(parent=parent)
+
+        self.canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
+        self.canvas.measure_fps()
+
+        self.view = self.canvas.central_widget.add_view()
+
+        self.volume = scene.visuals.Volume(data, parent=self.view.scene,
+                                           threshold=0.1,
+                                           emulate_texture=False)
+
+        self.view.camera = scene.cameras.TurntableCamera(parent=self.view.scene,
+                                                         fov=60.,
+                                                         name='Turntable')
 
 # Start up Qt application
 qapp = QtGui.QApplication([''])
@@ -12,3 +34,5 @@ data = np.arange(1000).reshape((10,10,10))
 # Set up widget
 w = QtVispyWidget(data=data)
 w.canvas.render()
+
+qapp.exec_()
