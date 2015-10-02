7a175ab7eaed31cb1685eefb9246c09b515f36f8
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-10-02 17:45:58 +0200

    Try again without emulate_texture=True

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ data = np.arange(1000).reshape((10, 10, 10))
 
 canvas = scene.SceneCanvas(show=True)
 view = canvas.central_widget.add_view()
-volume = scene.visuals.Volume(data, parent=view.scene, emulate_texture=True)
+volume = scene.visuals.Volume(data, parent=view.scene)
 view.camera = scene.cameras.TurntableCamera(parent=view.scene)
 
 canvas.render()
